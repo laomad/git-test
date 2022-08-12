ec91b7beb41067f4854a0d6d0750f120b04cf721
ma <3314991589@qq.com>
2022-08-12 22:58:22 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author:malinyang
+ * @Date:2022/8/12 22:48
+ * @Description:com.atguigu.git
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
